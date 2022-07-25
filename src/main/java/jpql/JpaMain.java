baa3dffec5f66f91f1edaa9c6674e536089d7402
kyubeom <be_man@naver.com>
2022-07-26 00:12:16 +0900

    조인 종류 (inner, left, seta)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -28,6 +28,30 @@ public class JpaMain {
             query.setParameter("username","memeber1");
             Member singleResult = query.getSingleResult();
 
+            //임베디드 타입 프로젝션
+            em.createQuery("select o.address from Order o",Address.class);
+
+            //스칼라 타입 프로젝션
+            em.createQuery("select m.username, m.age from Member m");
+
+            //페이징 API
+            List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(10)
+                    .setMaxResults(20)
+                    .getResultList();
+
+            //innerjoin
+            String query1 = "select m from Member m inner join m.team t";
+            em.createQuery(query1,Member.class);
+
+            //left outer join
+            String query2 = "select m from Member m left outer join m.team t";
+            em.createQuery(query2,Member.class);
+
+            //setajoin
+            String query3 = "select m from Member m, Team t where m.username = t.name";
+            em.createQuery(query3,Member.class);
+
             tx.commit();
         }catch (Exception e){
             tx.rollback();
@@ -35,12 +59,5 @@ public class JpaMain {
             em.close();
         }
         emf.close();
-    }
-    public static void  printMemeberAndTeam(Member member){
-        String username = member.getUsername();
-        System.out.println("username = " + username);
-
-        Team team = member.getTeam();
-        System.out.println("team = " + team.getName());
     }
 }
